f210da12a0fa1eac8e8a9c6ff3bb839feec1c545
ack2406 <ack9222@gmail.com>
2025-01-10 17:10:25 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+import ale_py
+
+def main() -> None:
+    gym.register_envs(ale_py)
+
+    env = gym.make("ALE/MontezumaRevenge-v5", render_mode="human")
+
+    observation, info = env.reset(seed=42)
+    for _ in range(1000):
+        action = env.action_space.sample()
+
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+
+if __name__ == "__main__":
+    main()
